60d1c2e815c2a808c429d5e2c7a6bf24a7648611
imanejb <imaane49@gmail.com>
2023-07-18 15:48:20 -0700

    betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_sign - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
@@ -16,7 +15,7 @@ int print_sign(int n)
 	return (1);
 }
 	else if (n < 0)
-{		
+{
 	_putchar('-');
 	return (-1);
 }
