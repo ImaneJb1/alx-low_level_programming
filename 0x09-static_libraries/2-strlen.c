94b34a7b2fe1847b072af4a47f6a0ce5bb452dde
ImaneJb1 <imaane49@gmail.com>
2023-09-02 01:14:50 -0700

    libmy.a

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,31 @@
+#include"main.h"
+/**
+ * _strlen - returns lenght of a string
+ * @s: string parameter output
+ * Return: the lenght of string
+*/
+int _strlen(char *s)
+{
+int strlen = 0;
+
+for (strlen = 0; *s != '\0'; s++)
+{
+strlen++;
+}
+return (strlen);
+#include"main.h"
+/**
+ * _strlen - returns lenght of a string
+ * @s: string parameter output
+ * Return: the lenght of string
+*/
+int _strlen(char *s)
+{
+int strlen = 0;
+
+for (strlen = 0; *s != '\0'; s++)
+{
+strlen++;
+}
+return (strlen);
+}}
